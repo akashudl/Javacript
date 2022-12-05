ccd4f5b93f0f88e6c323f16651bf4660d7111c39
Akash Pandit <78961715+akashudl@users.noreply.github.com>
2022-12-05 19:00:34 +0530

    Add files via upload
    Javascript tutorial

---
+++ Incrementdecrementcouter/main.js
@@ -0,0 +1,16 @@
+const counter=document.querySelectorAll('.counter');
+console.log(counter)
+counter.forEach((counters)=>{
+    counters.innerHTML=0;
+    const updatecounter=()=>{
+        const target=+counters.getAttribute('data-target')
+        const startincount=Number(counters.innerHTML);
+        const incr=target/100;
+        if(startincount<target)
+        {
+            counters.innerHTML=`${Math.round234w(startincount+incr)}`
+            setTimeout(updatecounter,10)
+        }
+    }
+    updatecounter();
+})
